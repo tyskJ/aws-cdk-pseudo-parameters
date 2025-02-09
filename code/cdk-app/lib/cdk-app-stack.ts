85c21d6d8252ae651628d793a9db86cd8bf11a1f
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-02-10 00:05:03 +0900

    add pseudo test resources

--- code/cdk-app/lib/cdk-app-stack.ts
+++ code/cdk-app/lib/cdk-app-stack.ts
@@ -1,16 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class CdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 擬似パラメータインスタンス作成
+    const pseudo = new cdk.ScopedAws(this);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // IAMポリシー作成
+    new iam.ManagedPolicy(this, "policy", {
+      managedPolicyName: "iam-policy",
+      statements: [
+        new iam.PolicyStatement({
+          sid: "testPolicy",
+          effect: iam.Effect.ALLOW,
+          actions: ["ec2:StartInstances"],
+          resources: [
+            `arn:${pseudo.partition}:ec2:${pseudo.region}:${pseudo.accountId}:instance/*`,
+          ],
+        }),
+      ],
+    });
   }
 }
